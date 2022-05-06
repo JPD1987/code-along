11d6db2b43254d2e071529ee58782feb6f5f68b4
Justin Desoto <justin.desoto@yahoo.com>
2022-05-05 21:08:24 -0600

    data types v3

---
+++ polarbears.js
@@ -0,0 +1,22 @@
+// const iceCapsAreMelting = true;
+// let polarBears = 5;
+//
+// // Initiate infinite loop
+// while (iceCapsAreMelting) {
+//     console.log(`There are ${polarBears} polar bears.`);
+//     polarBears--;
+//     // Terminate infinite loop when following condition is true
+//     if (polarBears === 0) {
+//         console.log("There are no polar bears left.");
+//         break;
+//     }
+// }
+// Initialize empty array
+let arrayExample = [];
+
+// Initialize loop to run 3 times
+for (let i = 0; i < 9; i++) {
+    // Update array with variable value
+    arrayExample.push(i);
+    console.log(arrayExample);
+}
